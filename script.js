39f107dff057d74395102342715e87d5232cd411
aniket sharma <aniket965.as@gmail.com>
2016-12-21 08:43:23 +0530

    completed index page task #3,#4

--- script.js
+++ script.js
@@ -5,5 +5,14 @@ $(window).scroll( function() {
 	if (wscroll> $('.playbutton').offset().top) {
 		$('.as').addClass('fade-in');
 	}
-
+   if(wscroll>$('.box').offset().top) {
+		$('.as1').addClass('fade-up');
+	} 
+	 if(wscroll>$('.as1').offset().top) {
+		$('.as2').addClass('fade-up');
+	} 
+	 if(wscroll-100>$('.as1').offset().top) {
+		$('.as3').addClass('fade-up');
+	} 
+	
 });
